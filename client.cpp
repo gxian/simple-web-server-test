20818e4dd8eb44e63f81b2cc1d6fdd0daab16769
gaoxiang <gaoxiang.ian@gmail.com>
2018-06-01 20:38:48 +0800

    完成Post json echo版本

--- client.cpp
+++ client.cpp
@@ -5,17 +5,30 @@
 
 using HttpClient = SimpleWeb::Client<SimpleWeb::HTTP>;
 
-int main() {
-    HttpClient client("localhost:8080");
+int main(int argc, char** argv) {
+    if (argc != 4) {
+        return -1;
+    }
+    std::string ip, port, count;
+    ip = argv[1];
+    port = argv[2];
+    count = argv[3];
+    std::string address = ip + ":" + port;
+    int cnt = std::atoi(count.c_str());
 
+    HttpClient client(address);
     std::string json_string =
         "{\"firstName\": \"John\",\"lastName\": \"Smith\",\"age\": 25}";
-    client.request("POST", "/json", json_string,
-                   [](std::shared_ptr<HttpClient::Response> response,
-                      const SimpleWeb::error_code &ec) {
-                       if (!ec)
-                           std::cout << response->content.rdbuf() << std::endl;
-                   });
+
+    for (int i = 0; i < cnt; ++i) {
+        client.request("POST", "/json", json_string,
+                       [](std::shared_ptr<HttpClient::Response> response,
+                          const SimpleWeb::error_code& ec) {
+                           if (!ec)
+                               std::cout << response->content.rdbuf()
+                                         << std::endl;
+                       });
+    }
     client.io_service->run();
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <memory>
+#include <string>
+#include "dep/simple_web_server/server_http.hpp"
 
-int main() {
-    return 0;
+using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        return -1;
+    }
+
+    HttpServer server;
+    server.config.port = static_cast<unsigned short>(std::atoi(argv[1]));
+
+    server.resource["^/json$"]["POST"] =
+        [](std::shared_ptr<HttpServer::Response> response,
+           std::shared_ptr<HttpServer::Request> request) {
+            try {
+                // echo
+                *response << "HTTP/1.1 200 OK\r\n"
+                          << "Content-Length: " << request->content.string().length() << "\r\n\r\n"
+                          << request->content.string();
+            } catch (const std::exception& e) {
+                *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: "
+                          << strlen(e.what()) << "\r\n\r\n"
+                          << e.what();
+            }
+
+            return 0;
+        };
+
 }
